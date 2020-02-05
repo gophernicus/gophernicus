2cc8d204cb4bea6029a994ed13890bcaa266c2f4
fosslinux <fosslinux@aussies.space>
2020-02-05 19:55:38 +1100

    Fix travis "testsuite" introduced in 4340e3c55b38af280823d8d9c5364919c47c61a8

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if uname | grep -q 'Darwin' ; then
+    # I do not have hardware to make this work on and am uninterested in
+    # making another testsuite for travis Macs at this time
+    exit 0
+fi
+
+sudo cp .travis/test.gophermap /var/gopher/gophermap
+sudo chmod 644 /var/gopher/gophermap
+
 echo -e "\n" | gophernicus -nf -nu -nv -nx -nf -nd > test.output
 if ! cmp .travis/test.answer test.output ; then
     exit 1
