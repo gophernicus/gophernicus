gophernicus/gophernicus

commit a00fa5330dd424d41dccfe35d159ee3ebb7d7d90

Fix URI inserting aribitary scripts
Fixes #118
Fixes #117

Encodes characters using html percent encoding.

I hope this is correct, I'm pretty sure it is, but can't be certain
right now.

src/gophernicus.c
@@ -107,6 +107,27 @@ void footer(state *st)
 	}
 }
 
+void html_encode(const char *unsafe, char *dest, int bufsize)
+{
+	char literals[] = "!#$&'()*+,/:;=?@[]-_.~";
+	int i = 0, j = 0;
+	while (unsafe[i] != '\0') {
+		if (j >= bufsize - 5) {
+			break;
+		}
+		if (strchr(literals, unsafe[i]) ||
+			(unsafe[i] >= 'a' && unsafe[i] <= 'z') ||
+			(unsafe[i] >= 'A' && unsafe[i] <= 'Z') ||
+			(unsafe[i] >= '0' && unsafe[i] <= '9')) {
+			dest[j] = unsafe[i];
+			i += 1;
+			j += 1;
+		} else {
+			j += snprintf(&dest[j], BUFSIZE - j, "%%%02x", unsafe[i]);
+			i += 1;
+		}
+	}
+}
 
 /*
  * Print error message & exit
@@ -134,13 +155,17 @@ void die(state *st, const char *message, const char *description)
 
 	/* Handle HTML errors */
 	else if (st->req_filetype == TYPE_HTML) {
+		char safe_message[BUFSIZE];
+		html_encode(message, safe_message, BUFSIZE);
+		char safe_description[BUFSIZE];
+		html_encode(description, safe_description, BUFSIZE);
 		printf("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">\n"
 			"<HTML>\n<HEAD>\n"
 			"  <META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html;charset=iso-8859-1\">\n"
 			"  <TITLE>" ERROR_PREFIX "%1$s %2$s</TITLE>\n"
 			"</HEAD>\n<BODY>\n"
 			"<STRONG>" ERROR_PREFIX "%1$s %2$s</STRONG>\n"
-			"<PRE>", message, description);
+			"<PRE>", safe_message, safe_description);
 		footer(st);
 		printf("</PRE>\n</BODY>\n</HTML>\n");
 	}

src/file.c
@@ -107,10 +107,13 @@ void send_text_file(state *st)
  */
 void url_redirect(state *st)
 {
-	char dest[BUFSIZE];
+	char unsafe[BUFSIZE];
 
 	/* Basic security checking */
-	sstrlcpy(dest, st->req_selector + 4);
+	sstrlcpy(unsafe, st->req_selector + 4);
+
+	char dest[BUFSIZE];
+	html_encode(unsafe, dest, BUFSIZE);
 
 	if (sstrncmp(dest, "http://") != MATCH &&
 		sstrncmp(dest, "https://") != MATCH &&

src/gophernicus.h
@@ -446,6 +446,7 @@ void info(state *st, char *str, char type);
 void footer(state *st);
 void die(state *st, const char *message, const char *description);
 void log_combined(state *st, int status);
+void html_encode(const char *unsafe, char *dest, int bufsize);
 
 /* file.c */
 void send_binary_file(state *st);

commit db7e82dfd31e80fa6c3c2357aa2a9ae3056866fc

Explicitly terminate dest string in html_encode
This fixes an issue introduced in commit a00fa5330dd4. As the
destination string produced by html_escape is not explicitly
terminated by the function as introduced in this commit, the
escaped URL in dest continues with characters found in memory
that have not been overwritten, which essentially breaks the
encoded URL. The behaviour prior to commit a00fa5330dd4 was as
follows:

    $ src/gophernicus <<<URL:https://example.com | grep Refresh
      <META HTTP-EQUIV="Refresh" content="1;URL=https://example.com">

The broken behaviour since commit a00fa5330dd4 is this:

    $ src/gophernicus <<<URL:https://example.com | grep Refresh
      <META HTTP-EQUIV="Refresh" content="1;URL=https://example.comtps://example.com"">

Explicitly terminating the destination string by adding a zero
byte seems to fix this; yet, since I am not a C programmer myself,
I may well have missed some ramifications of this change. In any
case, the output produced by gophernicus since commit a00fa5330dd4
is essentially broken, so this is probably still an improvement.

src/gophernicus.c
@@ -127,6 +127,7 @@ void html_encode(const char *unsafe, char *dest, int bufsize)
 			i += 1;
 		}
 	}
+	dest[j] = '\0';
 }
 
 /*